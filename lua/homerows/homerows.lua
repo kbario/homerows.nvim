224ea20f03837b2aad53c082c7adb24a11db0c5a
kbario <kylebario1@gmail.com>
2023-05-01 07:41:13 +0800

    asdf

--- lua/homerows/homerows.lua
+++ lua/homerows/homerows.lua
@@ -6,6 +6,8 @@ local prefs = utes.load_config()
 
 local layout = layouts[prefs["current_layout"]]
 for key, value in pairs(prefs["custom_keys"]) do
+    log.info(key, value)
+    log.info(layout[key], layout[value])
     layout[key] = layout[value]
 end
 
